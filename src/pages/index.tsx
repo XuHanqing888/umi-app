a13d06fe00d49555f844f5e4a31c54f646646c6d
Tesla <2291056859@qq.com>
2021-09-20 13:10:23 +0800

    回滚

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,9 @@
 import styles from './index.less';
 
-// export default function IndexPage() {
-//   return (
-//     <div>
-//       <h1 className={styles.title}>Page index</h1>
-//     </div>
-//   );
-// }
-import React, { Component } from 'react';
-
-class index extends Component {
-  state = { a: 1 };
-  render() {
-    return (
-      <div>
-        <h1>{this.state.a}</h1>
-      </div>
-    );
-  }
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
 }
-
-export default index;
